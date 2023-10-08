b17be449659c19df87d2e24bb0788789a6ad7fed
Raphael Suarez <raphaelsoaresbrasil@hotmail.com>
2023-10-08 19:33:44 -0300

    paginatiob

--- ct-project/dashboard/urls.py
+++ ct-project/dashboard/urls.py
@@ -2,5 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('redes-sociais', views.social_media, name='social_media'),
+    path('clientes', views.clients, name='clients'),
+    path('campanhas', views.campaigns, name='campaigns'),
+    path('contratos', views.contracts, name='contracts'),
+    path('membros', views.members, name='members'),
+    path('servicos', views.services, name='services'),
+    path('empresas', views.companies, name='companies'),
+    path('redes-sociais/adicionar-metrica', views.add_sm_metric, name='add_sm_metric'),
 ]
--- ct-project/dashboard/views.py
+++ ct-project/dashboard/views.py
@@ -6,3 +6,35 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'dashboard/index.html')
     
+
+@login_required(login_url='auth/login')
+def social_media(request):
+    return render(request, 'dashboard/sm_metric.html')
+
+@login_required(login_url='auth/login')
+def clients(request):
+    return render(request, 'dashboard/clients.html')
+
+@login_required(login_url='auth/login')
+def campaigns(request):
+    return render(request, 'dashboard/campaigns.html')
+
+@login_required(login_url='auth/login')
+def services(request):
+    return render(request, 'dashboard/services.html')
+
+@login_required(login_url='auth/login')
+def members(request):
+    return render(request, 'dashboard/members.html')
+
+@login_required(login_url='auth/login')
+def contracts(request):
+    return render(request, 'dashboard/contracts.html')
+
+@login_required(login_url='auth/login')
+def companies(request):
+    return render(request, 'dashboard/companies.html')
+
+@login_required(login_url='auth/login')
+def add_sm_metric(request):
+    return render(request, 'dashboard/add_sm_metric.html')
