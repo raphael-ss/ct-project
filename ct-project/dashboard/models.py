ceaf8108f2a7c66c4155207130d819fa748f9855
Raphael Suarez <raphaelsoaresbrasil@hotmail.com>
2023-11-08 18:09:44 -0300

    sm metrics

--- ct-project/dashboard/models.py
+++ ct-project/dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 class Client(models.Model):
@@ -13,3 +14,32 @@ class Client(models.Model):
     last_name = models.CharField(max_length=32)
     gender = models.CharField(max_length=1, choices=GENDER)
     
+    
+class SocialMediaMetric(models.Model):
+    #timestamp = models.TimeField
+    INSTAGRAM = 'IG'
+    LINKEDIN = 'LI'
+    FACEBOOK = 'FC'
+    TIKTOK = 'TK'
+    NETWORKS=[
+        (INSTAGRAM, "Instagram"),
+        (LINKEDIN, "LinkedIn"),
+        (FACEBOOK, "Facebook"),
+        (TIKTOK, "TikTok"),
+    ]
+    date = models.DateField(null=False, default=datetime.datetime.now)
+    network = models.CharField(max_length=2, choices=NETWORKS, null=False)
+    followers = models.IntegerField(null=False)
+    impressions = models.IntegerField()
+    reach = models.IntegerField()
+    engagement = models.IntegerField()
+    visits = models.IntegerField()
+    notes = models.CharField(max_length=100)
+
+    def __srt__(self):
+        return f"{self.network} - {self.date}"
+
+    class Meta:
+        ordering: ['-date']
+
+
