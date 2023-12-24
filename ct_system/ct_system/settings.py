61663cd9f76c18ef7e63f068d03cb95382b07abc
Raphael Suarez <raphaelsoaresbrasil@hotmail.com>
2023-12-24 11:44:20 -0300

    done

--- ct_system/ct_system/settings.py
+++ ct_system/ct_system/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    #os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'ct_system/static'),
     os.path.join(BASE_DIR, 'dashboard/utils/font'),
 ]
